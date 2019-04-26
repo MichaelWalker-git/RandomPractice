894bef0567b8fe6db3e3dbc086c99a28481d65bf
Michael Tran <michaelhuytran@gmail.com>
2019-04-25 21:44:08 -0700

    [Feature] - algo work

--- Algorithms/FreeCodeCamp.js
+++ Algorithms/FreeCodeCamp.js
@@ -8,3 +8,27 @@ function findElement(arr, func) {
 
 findElement([1, 2, 3, 4], num => num % 2 === 0);
 
+/**
+ *
+ * @param bool
+ * @returns {boolean}
+ */
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
+
+booWho(null);
+
+/**
+ * Capitalizes each word
+ * @param {string} str
+ * @returns {string}
+ */
+function titleCase(str) {
+  return str
+    .split(" ")
+    .map((word) => word.substring(0,1).toUpperCase() + word.substring(1, str.length).toLowerCase())
+    .join(" ");
+}
+
+titleCase("I'm a little tea pot");
